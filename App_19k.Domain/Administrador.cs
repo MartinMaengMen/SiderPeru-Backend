5d99191de5ac5dddb779555e6946b7aa53001ada
Martin Ng <u201520759@upc.edu.pe>
2020-01-11 21:03:07 -0500

    Nuevo proyecto

--- App_19k.Domain/Administrador.cs
+++
@@ -1,9 +0,0 @@
-namespace App_19k.Domain
-{
-    public class Administrador
-    {
-       public int AdministradorId { get; set; }       
-       public Usuario AdministradorNavigation { get; set; }
-       public bool? FAdminGeneral { get; set; }
-    }
-}
--- App_19k.Domain/Departamento.cs
+++
@@ -1,9 +0,0 @@
-namespace App_19k.Domain
-{
-    public class Departamento
-    {
-       public int DepartamentoId { get; set; }
-        public string NDepartamento { get; set; }
-
-    }
-}
